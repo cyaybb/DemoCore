dcb7f085e829759eea8d93dee6ab0b163659ea35
402290540@qq.com <402290540@qq.com>
2021-05-13 21:27:13 +0800

    Revert "我这是提交 暂存数据"
    This reverts commit b68d38ff03c34bba305ad6c8efc4bb1b3196e3f0.

--- Entity/Teacher.cs
+++ Entity/Teacher.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace Domain.Entity
@@ -16,6 +15,6 @@ namespace Domain.Entity
         public string Name { get; set; }
         public int Age { get; set; }
         public string Address { get; set; }
-        public ICollection<Student> Students { get; set; }
+        public List<Student> Students { get; set; }
     }
 }
